3b0c4ae2d312f56ed5a04c407215c0b214f44923
IvanYou98 <yuanfanyou@gmail.com>
2022-08-18 22:20:14 -0400

    only creater of the public post can remove the post

--- src/pages/PublicPost/PublicPost.js
+++ src/pages/PublicPost/PublicPost.js
@@ -1,4 +1,5 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList, IonTitle } from '@ionic/react'
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonFab, IonFabButton, IonIcon, IonList, IonTitle } from '@ionic/react'
+import { closeCircle } from "ionicons/icons";
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
@@ -7,6 +8,21 @@ import { BACKEND_HOST } from '../../constants'
 import './PublicPost.css'
 const PublicPost = ({ socket }) => {
     const [notes, setNotes] = useState([]);
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+
+
+    const btnRemoveHandler = async (noteId) => {
+        const response = await fetch(`${BACKEND_HOST}/public/${noteId}`, {
+            method: 'DELETE', headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        const deletedNote = await response.json();
+        socket.emit("newPublicPost", {
+            sender: currentUser.email
+        })
+        setNotes(notes.filter(note => note._id !== deletedNote._id))
+    }
 
     useEffect(() => {
         fetch(`${BACKEND_HOST}/public/`).then(
@@ -38,6 +54,19 @@ const PublicPost = ({ socket }) => {
                     notes.map(note => (
                         <IonCard key={note._id}>
                             <IonCardHeader>
+                                {
+                                    currentUser.uid === note.userId ? (
+                                        <IonFab vertical="top" horizontal="end">
+                                            <IonFabButton
+                                                color="danger"
+                                                onClick={() => btnRemoveHandler(note._id)}
+                                                size="small"
+                                                className="close-btn">
+                                                <IonIcon icon={closeCircle}></IonIcon>
+                                            </IonFabButton>
+                                        </IonFab>
+                                    ) : null
+                                }
                                 <IonCardTitle>
                                     {note.title}
                                 </IonCardTitle>
